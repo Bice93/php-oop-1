08addc90510c6ee7e9e41aadbd844f001c0f3b92
Bice93 <bianca.cerulo@hotmail.it>
2022-09-02 15:42:55 +0200

    Add the constructor

--- index.php
+++ index.php
@@ -3,10 +3,18 @@
 //Create a new class "Movie"
 class Movie{
 
-    //declare variables or attributes
+    //Declare variables or attributes
     private $title;
     private $year;
     private $duration;
+
+    //Define the constructor
+    function __construct($_title, $_year, $_duration)
+        {
+            $this->title = $_title;
+            $this->year = $_year;
+            $this->duration = $_duration;
+        }
 
 }
 
